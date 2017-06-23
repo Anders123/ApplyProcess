2480735543615f1a03d24b30f365b73552ad9921
Anders Hundborg <elteldmclient@gmail.com>
2017-06-23 14:20:14 +0200

    Reading from web api to get an interview.

--- ApplyProcess/Components/ApplyComponent.cs
+++ ApplyProcess/Components/ApplyComponent.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using ApplyProcess.Models;
 using Common;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 
 namespace ApplyProcess.Components
 {
@@ -20,15 +24,42 @@ namespace ApplyProcess.Components
 
         public async Task<IViewComponentResult> InvokeAsync(int id)
         {
+            var interview = await GetData();
 
             ApplyViewModel viewModel = null;
             await Task.Run(() =>
             {
                  viewModel = new ApplyViewModel();
+                viewModel.Interview = interview;
             });
             return View(viewModel);
         }
 
+        public async Task<Interview> GetData()
+        {
+            // http://localhost:62341/Interview/Create?briefCaseId=1
+
+            using (var client = new HttpClient())
+            {
+              //  client.BaseAddress = new Uri(_webApiSettings.BaseUrl + "Interview/Create?briefCaseId=1");  //new Uri("http://www.omdbapi.com");
+               // client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+               
+
+                HttpResponseMessage response = await client.PostAsync(new Uri(_webApiSettings.BaseUrl + "Interview/Create?briefCaseId=1", UriKind.Absolute), null);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var data = await response.Content.ReadAsStringAsync();
+                    var interview = JsonConvert.DeserializeObject<Interview>(data);
+                    return interview;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
 
     }
+
 }
--- ApplyProcess/Models/ApplyViewModel.cs
+++ ApplyProcess/Models/ApplyViewModel.cs
@@ -7,13 +7,14 @@ namespace ApplyProcess.Models
 {
     public class ApplyViewModel
     {
-        public int NumberOfQuestions { get; set; } = 3;
+        public Interview Interview { get; set; }
+        //public int NumberOfQuestions { get; set; } = 3;
 
-        public List<string> Questions = new List<string>
-        {
-            "Question 1",
-            "Question 2",
-            "Question 3"
-        };
+        //public List<string> Questions = new List<string>
+        //{
+        //    "Question 1",
+        //    "Question 2",
+        //    "Question 3"
+        //};
     }
 }
